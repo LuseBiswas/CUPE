fde13f38370f837d6ce65f93102ba774986fc9f9
LuseBiswas <lusebiswas@gmail.com>
2024-03-24 12:21:50 +0530

    Connection Frontend to Backend

--- frontend/frontend/src/Pages/Experience/Experience.jsx
+++ frontend/frontend/src/Pages/Experience/Experience.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import styles from './Experience.module.css'
 import axios from 'axios'
 
+import testImage from '/Users/riteshbiswas/Development/Personal/CUPE/frontend/frontend/public/assets/logo_copy.png';
+
 
 const Experience = () => {
 
@@ -29,7 +31,17 @@ const Experience = () => {
 
             {
               reviews.map((Item)=>(
-                <h6>{Item.CompnayTitle}</h6>
+                <div className={styles.CupesBox}>
+                   <img src={testImage} alt=""/>
+
+                   <div className={styles.CupesBoxContent}>
+                   <h6>{Item.CompnayTitle}</h6>
+                  <h5>Author: <span>{Item.Reviewer}</span></h5>
+
+                  <p>Placement Year: {Item.PlacementYear}</p>
+
+                   </div>
+                </div>
               ))
             }
           </div>
