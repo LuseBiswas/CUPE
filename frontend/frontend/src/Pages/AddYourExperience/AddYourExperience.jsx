dcb537aa319f600f25ecdc1bc1c654ef07ab752c
LuseBiswas <lusebiswas@gmail.com>
2024-03-31 12:26:25 +0530

    Adding your Experince

--- frontend/frontend/src/Pages/AddYourExperience/AddYourExperience.jsx
+++ frontend/frontend/src/Pages/AddYourExperience/AddYourExperience.jsx
@@ -1,36 +1,70 @@
 import React, { useState } from 'react'
 import styles from './AddYourExperience.module.css';
+
+import { useSnackbar } from 'notistack';
+import { useNavigate } from 'react-router-dom';
+import axios, { Axios } from 'axios'
+
+
+
+
 const AddYourExperience = () => {
-    const [CompanyTitle, setCompanyTitle] = useState('');
-    const [Reviewer, setReviewer] = useState('');
-    const [CompanyImg_URL, setCompanyImg_URL] = useState('');
-    const [PlacementExp, setPlacementExp] = useState('');
-    const [CompanyCTC, setCompanyCTC] = useState('');
-    const [PlacementYear, setPalcementYear] = useState('');
-    
-    const SaveExp = () =>{};
+  const [CompanyTitle, setCompanyTitle] = useState('');
+  const [Reviewer, setReviewer] = useState('');
+  const [CompanyImg_URL, setCompanyImg_URL] = useState('');
+  const [PlacementExp, setPlacementExp] = useState('');
+  const [CompanyCTC, setCompanyCTC] = useState('');
+  const [PlacementYear, setPalcementYear] = useState('');
+
+  const [Loading, setLoading] = useState(false);
+
+
+
+  const Navigate = useNavigate()
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const SaveExp = () => {
+
+    const data = {
+      CompanyTitle, Reviewer, CompanyImg_URL, PlacementExp, CompanyCTC, PlacementYear
+    }
+
+    setLoading(true);
+    axios.post('http://localhost:5555/cupes', data).then(() => {
+      setLoading(false);
+      enqueueSnackbar("New Experience Saved Succesfully ", { variant: "success" })
+      Navigate("/")
+
+    }).catch((error) => {
+      setLoading(false);
+      enqueueSnackbar("Something went wrong ", { variant: "error" })
+      console.log(error);
+    })
+
+  };
 
   return (
     <>
-    <section className={styles.AddYourExperience_Main}>
-        <div className={styles.AddYourExperience_Row}>
-            <h6>Add your Experience</h6>
+      <section className={styles.AddYourExperience_Main}>
+        <div className={styles.AddYourExperience_MainRow}>
+          <h6>Add your Experience</h6>
 
-            <input type="text" placeholder='Company Name' value={CompanyTitle} onChange={(e)=> setCompanyTitle(e.target.value)} name='' id='' />
-            
-            <input type="text" placeholder='Reviewer Name' value={Reviewer} onChange={(e)=> setReviewer(e.target.value)} name='' id='' />
-            
-            <input type="text" placeholder='Company Img URL' value={CompanyImg_URL} onChange={(e)=> setCompanyImg_URL(e.target.value)} name='' id='' />
+          <input type="text" placeholder='Company Name' value={CompanyTitle} onChange={(e) => setCompanyTitle(e.target.value)} name='' id='' />
 
-            <input type="text" placeholder='Placement Experience' value={PlacementExp} onChange={(e)=>setPlacementExp(e.target.value)} name='' id='' />
+          <input type="text" placeholder='Reviewer Name' value={Reviewer} onChange={(e) => setReviewer(e.target.value)} name='' id='' />
 
-            <input type="text" placeholder='Company CTC' value={CompanyCTC} onChange={(e)=>setCompanyCTC(e.target.value)} name='' id='' />
-            
-            <input type="text" placeholder='Placement Year' value={PlacementYear} onChange={(e)=>setPalcementYear(e.target.value)} name='' id='' />
+          <input type="text" placeholder='Company Img URL' value={CompanyImg_URL} onChange={(e) => setCompanyImg_URL(e.target.value)} name='' id='' />
 
-            <button className={styles.btn} onClick={SaveExp}>Save Experience</button>
+          <textarea type="text" placeholder='Placement Experience' value={PlacementExp} onChange={(e) => setPlacementExp(e.target.value)} name='' id='' />
+
+          <input type="text" placeholder='Company CTC' value={CompanyCTC} onChange={(e) => setCompanyCTC(e.target.value)} name='' id='' />
+
+          <input type="number" placeholder='Placement Year' value={PlacementYear} onChange={(e) => setPalcementYear(e.target.value)} name='' id='' />
+
+          <button className={styles.btn} onClick={SaveExp}>Save Experience</button>
         </div>
-    </section>
+      </section>
     </>
   )
 }
--- frontend/frontend/src/Pages/Experience/Experience.jsx
+++ frontend/frontend/src/Pages/Experience/Experience.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import styles from './Experience.module.css'
 import { Link } from 'react-router-dom';
-import axios from 'axios'
+import axios from 'axios';
+
 
 import Spinner from '../../Components/Spinner/Spinner';
 
