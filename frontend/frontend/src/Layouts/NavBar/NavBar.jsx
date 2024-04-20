1a74e4d3681a19f47b6ed106926cd02e51bc8c64
LuseBiswas <lusebiswas@gmail.com>
2024-04-20 11:34:42 +0530

    nav bar is finally fixed and responsive

--- frontend/frontend/src/Layouts/NavBar/NavBar.jsx
+++ frontend/frontend/src/Layouts/NavBar/NavBar.jsx
@@ -1,43 +1,58 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NavBar.module.css'
 import { Link } from 'react-router-dom';
 import Logo from '../../../public/assets/CUPEF.png';
+import { CgMenu, CgClose } from 'react-icons/cg'
 
 const NavLink = [
     {
-        LinkText:"Home",
-        LinkTo:"/",
+        LinkText: "Home",
+        LinkTo: "/",
     },
     {
-        LinkText:"About",
-        LinkTo:"/About",
+        LinkText: "About",
+        LinkTo: "/About",
     },
     {
-        LinkText:"Experience",
-        LinkTo:"/Experience",
+        LinkText: "Experience",
+        LinkTo: "/Experience",
     },
 ]
 
+
+
 const NavBar = () => {
-  return (
-    <>
-    <nav className={styles.NavbarMain}>
-        <Link to={'/'}>
-            <img src={Logo} alt="Logo" />
-        </Link>
 
+    const [openMenue, setOpenMenue] = useState(false)
+    return (
+        <>
+            <nav className={styles.NavbarMain}>
+                <Link to={'/'}>
+                    <img src={Logo} alt="Logo" />
+                </Link>
 
-        <div className={styles.NavLinks}>
-            {
-                NavLink.map((Item, index) =>(
-                    <Link key={index} to={Item.LinkTo}>{Item.LinkText}</Link>
-                ))
-            }
-        </div>
-    </nav>
-    </>
-  )
+                <div className={styles.menue} onClick={() => {
+                    setOpenMenue(!openMenue);
+                }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <ul className={openMenue ? styles.open : ""}>
+                    <li>
+                        {
+                            NavLink.map((Item, index) => (
+                                <Link key={index} to={Item.LinkTo}>{Item.LinkText}</Link>
+                            ))
+                        }
+                    </li>
+                </ul>
+
+            </nav>
+        </>
+    )
 }
 
 export default NavBar
