ee88f2ce7c6d52afacfe5d2bd9b2166042a9eecf
LuseBiswas <lusebiswas@gmail.com>
2024-04-21 13:46:16 +0530

    Upcoming Feture is implemented and fully responsive

--- frontend/frontend/src/Pages/Home/Home.jsx
+++ frontend/frontend/src/Pages/Home/Home.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import styles from './Home.module.css';
+import Marquee from 'react-fast-marquee';
+//Importing Images
+import img1 from '../../../public/assets/UpComingFeatures/Image1.png';
+import img2 from '../../../public/assets/UpComingFeatures/Image2.png';
+import img3 from '../../../public/assets/UpComingFeatures/Image3.png';
+import img4 from '../../../public/assets/UpComingFeatures/Image4.png';
+import img5 from '../../../public/assets/UpComingFeatures/Image5.png';
 import Hero1 from '../../../public/assets/HeroImg/Final.png';
 //For Components
 import Button from '../../Components/Button/Button';
@@ -22,6 +29,28 @@ const Home = () => {
             <img src={Hero1} alt="" />
         </div>
     </section>
+    <div className={styles.Features}>
+          <h6>Upcoming Features</h6>
+          <div>
+            <Marquee loop={20} speed={120} autoFill={true}>
+              <div className={styles.imageWrapper}>
+                <img src={img1} alt="" />
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+                <img src={img4} alt="" />
+                <img src={img5} alt="" />
+
+                <img src={img1} alt="" />
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+                <img src={img4} alt="" />
+                <img src={img5} alt="" />
+                
+              </div>
+            </Marquee>
+          </div>
+
+        </div>
     </>
   )
 }
