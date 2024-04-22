f895aa62e4d05dd75079c05b07ca52590b6a49bc
LuseBiswas <lusebiswas@gmail.com>
2024-04-23 00:09:05 +0530

    AddYourExp button Style Changed

--- frontend/frontend/src/Pages/Experience/Experience.jsx
+++ frontend/frontend/src/Pages/Experience/Experience.jsx
@@ -14,67 +14,72 @@ const Experience = () => {
   const [loading, setLoading] = useState(false);
   const [SearchinputValue, setSearchInputValue] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
 
     setLoading(true);
-    axios.get('https://cupe-ritesh-biswas-projects.vercel.app/cupes').then(res =>{
-    setReviews(res.data.data)
-    setLoading(false);
-    }).catch((error)=>{
+    axios.get('https://cupe-ritesh-biswas-projects.vercel.app/cupes').then(res => {
+      setReviews(res.data.data)
+      setLoading(false);
+    }).catch((error) => {
       console.log(error);
       setLoading(false);
     })
-  },[])
+  }, [])
 
   //Filter Experience
 
-  const fillterData = reviews.filter((items)=>{
+  const fillterData = reviews.filter((items) => {
     return items.CompanyTitle.toLowerCase().includes(SearchinputValue.toLowerCase());
   })
   return (
     <>
 
-    {
-      loading ? (<Spinner/>) : (
-        
-        <div className={styles.CupesMain}>
+      {
+        loading ? (<Spinner />) : (
 
-<div className={styles.mainSearchBox}>
+          <div className={styles.CupesMain}>
+
+            <div className={styles.mainSearchBox}>
 
 
-          <div className={styles.searchContainer}>
-            <input type="text" placeholder='Search Company' value={SearchinputValue} autoComplete='off' onChange={(e)=>{setSearchInputValue(e.target.value)}}/>
+              <div className={styles.searchContainer}>
+                <input type="text" placeholder='Search Company' value={SearchinputValue} autoComplete='off' onChange={(e) => { setSearchInputValue(e.target.value) }} />
+              </div>
+            </div>
+
+            <div className={styles.AddyourExp}>
+
+            <Link to={"/AddYourExperience"} className={styles.AddyourExp2}>Add your Experience</Link>
+
+            </div>
+
+
+
+
+            <div className={styles.CupesMain_Row}>
+
+
+              {
+                fillterData.map((Item) => (
+                  <div className={styles.CupesBox}>
+                    <img src={Item.CompanyImg_URL} alt={Item.CompnayTitle} />
+
+                    <div className={styles.CupesBoxContent}>
+                      <h6>{Item.CompnayTitle}</h6>
+                      <p>{Item.PlacementExp}</p>
+                      <Link className={styles.Btn} to={`/Read/${Item._id}`}>Reead More</Link>
+
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
           </div>
-          </div>
-
-          
-          <Link to={"/AddYourExperience"} className={styles.AddyourExp}>Add your Experience</Link>
-
-
-          <div className={styles.CupesMain_Row}>
-            
-
-            {
-              fillterData.map((Item)=>(
-                <div className={styles.CupesBox}>
-                   <img src={Item.CompanyImg_URL} alt={Item.CompnayTitle}/>
-
-                   <div className={styles.CupesBoxContent}>
-                   <h6>{Item.CompnayTitle}</h6>
-                   <p>{Item.PlacementExp}</p>
-                   <Link className={styles.Btn} to={`/Read/${Item._id}`}>Reead More</Link>
-
-                   </div>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-      )
-    }
+        )
+      }
     </>
 
-    
+
   )
 }
 
